d745e301f8207d34e26fc187a56d9dd18df362c9
shashwat1608 <srivastavashashwat36@gmail.com>
2023-07-13 11:27:56 +0530

    first commit

--- setup.py
+++ setup.py
@@ -1,24 +1,26 @@
 from setuptools import find_packages,setup
-from typing import List 
+from typing import List
 
+HYPEN_E_DOT='-e .'
 
-Hyphen_E_Dot='-e .'
 def get_requirements(file_path:str)->List[str]:
     requirements=[]
     with open(file_path) as file_obj:
         requirements=file_obj.readlines()
         requirements=[req.replace("\n","") for req in requirements]
-        if Hyphen_E_Dot in requirements:
-            requirements.remove(Hyphen_E_Dot)
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+
         return requirements
 
+
 setup(
-    name='DiamondPricePrediction',
+    name='diamondpredictionproject',
     version='0.0.1',
-    author='shashwat',
+    author='Shashwat',
     author_email='srivastavashashwat36@gmail.com',
     install_requires=get_requirements('requirements.txt'),
-    packages=find_packages() 
-    
-    
+    packages=find_packages()
+
 )
